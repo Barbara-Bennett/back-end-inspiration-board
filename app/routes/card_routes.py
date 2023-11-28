258865eb5d61f602f22b1b9fdafc5d4f026012d4
Barbara <barbaraelisabethb@gmail.com>
2023-11-28 17:24:20 -0500

    Update card_routes

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -3,26 +3,46 @@ from app import db
 from app.models.card import Card
 from .routes_helpers import validate_model
 
-
+# example_bp = Blueprint('example_bp', __name__)
 card_bp = Blueprint("cards", __name__, url_prefix="/cards")
 
 
-@card_bp.route("/<card_id>", methods=["DELETE"])
-def delete_one_card(card_id):
-    card = validate_model(Card, card_id)
+@card_bp.route("/<id>", methods=["DELETE"])
+def delete_one_card(id):
+    card = validate_model(Card, id)
 
     db.session.delete(card)
     db.session.commit()
-    
     return make_response(jsonify({"details": f'Card {card.card_id} successfully deleted'}), 200)
 
-
-@card_bp.route("/<card_id>/add_like", methods=["PATCH"])
-def add_like(card_id):
-    card = validate_model(Card, card_id)
+# ADD LIKE TO CARD
+@card_bp.route("/<id>/add_like", methods=["PATCH"])
+def add_like(id):
+    card = validate_model(Card, id)
 
     card.likes_count += 1
     db.session.commit()
 
+    return make_response(jsonify({"card like count": card.likes_count}), 200)
 
-    return make_response(jsonify({"card_like_count": card.likes_count}), 200)
+# create a route to add new card for selected board
+@card_bp.route("", methods=["POST"])
+def create_card():
+    request_body = request.get_json()
+
+    try:
+        new_card = Card.from_dict(request_body)
+    except KeyError:
+        abort(make_response(jsonify({"details": "Invalid data"}), 400))
+
+    db.session.add(new_card)
+    db.session.commit()
+
+    return make_response(jsonify(new_card.to_dict()), 201)
+
+# create a route to view all cards for selected board
+@card_bp.route("", methods=["GET"])
+def get_all_cards():
+    cards = Card.query.all()
+    cards_response = [card.to_dict() for card in cards]
+    return make_response(jsonify(cards_response), 200)
